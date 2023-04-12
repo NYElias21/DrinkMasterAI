f4961dd61ea0959ae27663f8d5bf6550290e8823
Elias <nyelias21@gmail.com>
2023-04-12 02:24:42 -0400

    hero v1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import TabBar from './TabBar';
-import logo from './logo.svg';
+import logo from './logowhiskey.png';
 import './App.css';
+import Hero from './hero';
 
 
 function App() {
   return (
     <div className="App">
       <TabBar />
+      <Hero />
     </div>
   );
 }
---
+++ src/hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="Hero"> 
+        <h1>Create a new drink</h1>
+        <form className="SearchBar">
+            <input type="text" placeholder="Search..."/>
+        </form>
+    </div>
+  );
+}
+
+export default Hero;
