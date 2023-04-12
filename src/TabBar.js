31542eca56d3495014caefdc89a89ecb5947f3ef
Elias <nyelias21@gmail.com>
2023-04-12 12:05:32 -0400

    Hero Line + grids

--- src/TabBar.js
+++ src/TabBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logowhiskey.png';
 
 function TabBar() {
     return (
--- src/hero.js
+++ src/hero.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import logo from './logowhiskey.png';
 
+/*
 function Hero() {
   return (
     <div className="Hero"> 
@@ -12,3 +14,23 @@ function Hero() {
 }
 
 export default Hero;
+
+*/
+
+function Hero() {
+    return (
+      <div className="Hero">
+        <div className="Hero-left">
+          <h1>BECOME A MIXOLOGY MASTER </h1>
+          <form className="SearchBar">
+            <input type="text" placeholder="Search..." />
+          </form>
+        </div>
+        <div className="Hero-right">
+          <img src={logo} alt="Logo" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default Hero;
